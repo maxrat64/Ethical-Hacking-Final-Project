647f1b966e9048a0c52bdd09b096982c3bf8a2ad
Austin Aurelio <austin@delveam.com>
2021-04-25 18:46:17 -0500

    decided to make a web scraper instead

--- search.py
+++ search.py
@@ -1,29 +1,27 @@
-import os
-import json
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import quote
 
-# TODO: Test file enumeration on slower machine to see if performance is good
-# enough.
+def _getCVEData(url):
+    pass
 
-# Updates the database from the git repo. Should be called once at start of
-# program.
-def updateDatabase():
-    if (os.path.isdir("./cvelist")):
-        os.system("cd ./cvelist; git pull")
-    else:
-        os.system("git clone https://github.com/CVEProject/cvelist.git")
+def search(query: str):
+    url = "https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=" + quote(query)
+    page = requests.get(url)
 
-# Executes a search from the database given a query. The output is an
-# array of JSON objects as python dicts.
-def search(query: str) -> list[dict]:
-    if (os.path.isdir("./cvelist")):
-        raise FileNotFoundError("Database not found! Call updateDatabase() first!")
+    soup = BeautifulSoup(page.text, "html.parser")
 
-    result = []
-    for root, _, files in os.walk("./cvelist"):
-        for name in files:
-            path = os.path.join(root, name)
-            if path[-5:] == ".json":
-                with open(path) as f:
-                    json.load(f)
-                    # TODO: Create some kind of search metric to find matches
-    return result
+    table = soup.find_all("table")[2]
+
+    for child in table.children:
+        for td in child:
+            if hasattr(td, "a"):
+                if td.a != None:
+                    start = str(td.a).index("\"") + 1
+                    end = str(td.a).rindex("\"")
+                    ext = str(td.a)[start:end]
+                    url = "https://cve.mitre.org" + ext
+                    data = _getCVEData(url)
+                    # TODO: Scrape data out of CVE URLs
+
+# search("OpenSSH 6.1.1")
