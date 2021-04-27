a18d89b6e10c3026c5031f64ed1d8263e06c7f97
Austin Aurelio <austin@delveam.com>
2021-04-27 12:04:51 -0500

    implemented scraping of CVE entries

--- search.py
+++ search.py
@@ -2,16 +2,44 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import quote
 
-def _getCVEData(url):
-    pass
+def _getNVDData(url: str) -> dict:
+    # TODO: Scrape data from NVD (url obtained in CVD webpage)
+    return dict()
 
-def search(query: str):
+def _getCVEData(url: str) -> dict:
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    table = soup.find_all("table")[2].contents
+
+    DESC_IDX = 7
+    REF_IDX = 13
+    CNA_IDX = 17
+    DATE_IDX = 21
+
+    description = table[DESC_IDX].contents[1].text.strip()
+
+    references = []
+    tags_references = table[REF_IDX].contents[1].find_all("a")
+    for tag in tags_references:
+        references.append(tag.text)
+
+    cna_assigned = table[CNA_IDX].contents[1].text
+    date_assigned = table[DATE_IDX].contents[1].text
+
+    return {"description" : description, "references" : references, \
+            "cna": cna_assigned, "date": date_assigned}
+
+def _getData(url: str) -> dict:
+    result = _getCVEData(url)
+    result.update(_getNVDData(url))
+    return result
+
+def search(query: str) -> list[dict]:
     url = "https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=" + quote(query)
     page = requests.get(url)
-
     soup = BeautifulSoup(page.text, "html.parser")
-
     table = soup.find_all("table")[2]
+    result = []
 
     for child in table.children:
         for td in child:
@@ -22,6 +50,8 @@ def search(query: str):
                     ext = str(td.a)[start:end]
                     url = "https://cve.mitre.org" + ext
                     data = _getCVEData(url)
-                    # TODO: Scrape data out of CVE URLs
+                    result.append(data)
 
-# search("OpenSSH 6.1.1")
+    return result
+
+print(_getCVEData("https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-29262"))
