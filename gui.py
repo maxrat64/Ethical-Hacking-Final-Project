34757318b7708f40fb46786c30e3196e0f4a868d
Max Rattray <maxrat964@gmail.com>
2021-05-05 01:07:00 -0400

    more gui updates

--- gui.py
+++ gui.py
@@ -3,17 +3,37 @@ import webbrowser
 
 
 def display(d, l):
-    # create root window
-    window = Tk()
-    window.title("Vulnerama")
-    window.geometry("1200x500")
-    scrollbar = Scrollbar(window)
-    scrollbar.pack(side=LEFT, fill=Y)
-    listbox = Listbox(window, yscrollcommand=scrollbar.set, width=50)
+
+    # repopulate display when a new CPE is selected
+    def select(evt):
+        new_cpe = str(listbox.get(listbox.curselection()))
+        new_entry = cpes[new_cpe]
+        header.config(text=new_cpe)
+        new_ips = "IP:port pairs running service: \n"
+        for ip in new_entry["ips"][:-1]:
+            new_ips = new_ips + ip + "\n"
+        new_ips = new_ips + new_entry["ips"][-1]
+        ips.config(text=new_ips)
+        new_desc = ""
+        new_desc = new_desc + "Number of vulnerabilities: " + \
+            str(new_entry["results"]["n_vulns"]) + "\n"
+        new_desc = new_desc + "Highest CVSS severity score: " + \
+            str(new_entry["results"]["max"]) + "\n"
+        new_desc = new_desc + "Average CVSS severity score: " + \
+            str(round(new_entry["results"]["avg"], 1)) + "\n"
+        desc.config(text=new_desc)
+	url_button = Button(master=frame, text="Learn More", command=open_url)
+	url_button.pack()
+
+
+    def open_url():
+        cpe = cur_id.cget("text")
+        url = cpes[cpe]["results"]["url"]
+        webbrowser.open(url)
 
     def populate():
         listbox.delete(0, END)
-        print("populating data")
+	cpe = {}
         with l:
             # parse input
             for ip in d.keys():
@@ -23,9 +43,7 @@ def display(d, l):
                     for spec in port_block.keys():
                         result_dict = port_block[spec]
                         if spec in cpes.keys():
-                            host_port = str(ip) + ":" + str(port)
-                            if host_port not in cpes[spec]["ips"]:
-                                cpes[spec]["ips"].append(str(ip) + ":" + str(port))
+                            cpes[spec]["ips"].append(str(ip) + ":" + str(port))
                         else:
                             cpes[spec] = {}
                             cpes[spec]["ips"] = [str(ip) + ":" + str(port)]
@@ -34,8 +52,16 @@ def display(d, l):
         # populate listbox
         for cpe in cpes.keys():
             listbox.insert(END, str(cpe))
-
         window.after(5000, populate)
+
+
+    # create root window
+    window = Tk()
+    window.title("Vulnerama")
+    window.geometry("1200x500")
+    scrollbar = Scrollbar(window)
+    scrollbar.pack(side=LEFT, fill=Y)
+    listbox = Listbox(window, yscrollcommand=scrollbar.set, width=50, font=("Consolas", 20))
 
     cpes = {}
     populate()
@@ -47,56 +73,18 @@ def display(d, l):
     frame.pack()
     cpes_keys = cpes.keys()
     value_iterator = iter(cpes_keys)
-    # default_cpe = next(value_iterator)
-    default_cpe = "blah"
-    # default_entry = cpes[default_cpe]
-    cur_id = Label(master=frame, text=str(default_cpe))
-    cur_id.pack()
-    ips_text = "IP:port pairs running service: \n"
-    # for ip in default_entry["ips"][:-1]:
-    #    ips_text = ips_text + ip + "\n"
-    # ips_text = ips_text + default_entry["ips"][-1]
-    cur_ips = Label(master=frame, text=ips_text, wraplength=500)
-    cur_ips.pack()
-    desc_text = ""
-    # desc_text = desc_text + "Number of vulnerabilities: " + \
-    #    str(default_entry["results"]["n_vulns"]) + "\n"
-    # desc_text = desc_text + "Highest CVSS severity score: " + \
-    #    str(default_entry["results"]["max"]) + "\n"
-    # desc_text = desc_text + "Average CVSS severity score: " + \
-    #    str(round(default_entry["results"]["avg"], 1)) + "\n"
-    # desc_text = desc_text + "Learn more: " + default_entry["results"]["url"]
-    cur_desc = Label(master=frame, text=desc_text, wraplength=500)
-    cur_desc.pack()
+    header_text = "Vulnerama"
+    header = Label(master=frame, text=str(header_text), font=("Consolas", 30))
+    header.pack()
 
-    # repopulate display when a new CPE is selected
-    def select(evt):
-        new_cpe = str(listbox.get(listbox.curselection()))
-        new_entry = cpes[new_cpe]
-        cur_id.config(text=new_cpe)
-        new_ips = "IP:port pairs running service: \n"
-        for ip in new_entry["ips"][:-1]:
-            new_ips = new_ips + ip + "\n"
-        new_ips = new_ips + new_entry["ips"][-1]
-        cur_ips.config(text=new_ips)
-        new_desc = ""
-        new_desc = new_desc + "Number of vulnerabilities: " + \
-            str(new_entry["results"]["n_vulns"]) + "\n"
-        new_desc = new_desc + "Highest CVSS severity score: " + \
-            str(new_entry["results"]["max"]) + "\n"
-        new_desc = new_desc + "Average CVSS severity score: " + \
-            str(round(new_entry["results"]["avg"], 1)) + "\n"
-        # new_desc = new_desc + "Learn more: " + new_entry["results"]["url"]
-        cur_desc.config(text=new_desc)
+    ips_text = "Select a CPE from the list"
+    ips = Label(master=frame, text=ips_text, wraplength=500, font=("Consolas", 20))
+    ips.pack()
 
-    def open_url():
-        cpe = cur_id.cget("text")
-        url = cpes[cpe]["results"]["url"]
-        webbrowser.open(url)
+    desc_text = "<Waiting for selection>"
+    desc = Label(master=frame, text=desc_text, wraplength=500, font=("Consolas", 20))
+    desc.pack()
 
     listbox.bind('<<ListboxSelect>>', select)
-
-    url_button = Button(master=frame, text="Learn More", command=open_url)
-    url_button.pack()
 
     window.mainloop()
--- main.py
+++ main.py
@@ -36,8 +36,12 @@ if __name__ == '__main__':
             procs.append(p)
             p.start()
 
-        # join processses to end
-        for p in procs:
-            p.join()
+	gui.join()
 
-        gui.join()
+        # terminate running processes
+        for p in procs:
+            p.terminate()
+
+	exit()
+		
+
